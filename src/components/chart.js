1614ecbc28bc409600f26579785710c4daee4739
Benny Johansson <benny.json@gmail.com>
2018-01-09 17:45:40 +0000

    feat(WeatherList): add avg line

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data) {
+  const sum = data.reduce((value, sum) => value + sum, 0);
+
+  return  (sum / data.length).toFixed(0);
+}
 
 export default (props) => {
   return (
     <div>
       <Sparklines svgHeight={100} svgWidth={180} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>{average(props.data)}</div>
     </div>
   );
 }
