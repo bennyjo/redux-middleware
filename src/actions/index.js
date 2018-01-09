f5319a3c6628dd0aae8129d65d16d87b51700784
Benny Johansson <benny.json@gmail.com>
2018-01-09 17:45:40 +0000

    fix(actions): correct root url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
+import axios from 'axios';
+
 const API_KEY = '1e04f6423ee9dfaaea58f6adeb6c4fec';
-const ROOT_URL = `http://api.openweathermap.org/data/2,5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FEATCH_WEATHER = 'FETCH_WEATHER';
 
