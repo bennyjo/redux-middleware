846746c0120f8edcc5bf51c5a47e11dc4fd91a3f
Benny Johansson <benny.json@gmail.com>
2018-01-09 17:45:40 +0000

    feat(app): add units

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -13,13 +13,13 @@ class WeatherList extends Component {
       <tr key={city}>
         <td>{city}</td>
         <td>
-          <Chart data={temperatures} color='orange' />
+          <Chart data={temperatures} color='orange' unit='K'/>
         </td>
         <td>
-          <Chart data={pressures} color='blue' />
+          <Chart data={pressures} color='blue' unit='hPa' />
         </td>
         <td>
-          <Chart data={humidities} color='red ' />
+          <Chart data={humidities} color='red' unit='%' />
         </td>
       </tr>
     )
@@ -31,9 +31,9 @@ class WeatherList extends Component {
           <thead>
             <tr>
               <th>City</th>
-              <th>Temperature</th>
-              <th>Pressure</th>
-              <th>Humidity</th>
+              <th>Temperature (K)</th>
+              <th>Pressure (hPa)</th>
+              <th>Humidity (%)</th>
             </tr>
           </thead>
           <tbody>
