b9372d072283cc0147010595f07028f8e873c911
Benny Johansson <benny.json@gmail.com>
2018-01-09 17:45:40 +0000

    feat(actions): add featchWeather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 const API_KEY = '1e04f6423ee9dfaaea58f6adeb6c4fec';
+const ROOT_URL = `http://api.openweathermap.org/data/2,5/forecast?appid=${API_KEY}`;
+
+export const FEATCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},GB`;
+  const request = axios.get(url);
+
+  return {
+    type: FEATCH_WEATHER,
+    payload: request
+  };
+}
